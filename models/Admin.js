760eb325ffc5489a2c39ef97fa6392ed50a35dac
tuantu2609 <tuantums10@gmail.com>
2024-11-15 13:22:28 +0700

    Add video retrieval by course ID and implement admin model with unique constraints

---
+++ models/Admin.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+    const Admin = sequelize.define("Admin", {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+      }
+    });
+
+    Admin.beforeCreate(async (admin, options) => {
+        const count = await Admin.count();
+        if (count > 0) {
+          throw new Error("Only one admin account is allowed.");
+        }
+      });
+
+    return Admin;
+};
